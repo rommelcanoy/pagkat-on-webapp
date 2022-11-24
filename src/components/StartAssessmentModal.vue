904a929272f348a72f4f2e3a42a61f37e0ca7603
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2022-11-24 23:50:14 +0800

    chore: changed to select option in start assessment

--- src/components/StartAssessmentModal.vue
+++ src/components/StartAssessmentModal.vue
@@ -36,15 +36,18 @@ export default defineComponent({
           <span class="sr-only">Close modal</span>
         </button>
         <div class="py-6 px-6 lg:px-8">
-          <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Enter Student ID</h3>
+          <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Select assessment</h3>
           <form class="space-y-6" action="/dashboard/start-assessment">
-            <div>
-              <!-- <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enrollment 
-                  code</label> -->
-              <input type="text" name="enrollment_code" id="enrollment_code"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                placeholder="ID" required>
-            </div>
+              <!-- <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an
+                option</label> -->
+            <select id="countries"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <option selected>Choose an assessment</option>
+              <option value="1">Assessment 1</option>
+              <option value="2">Assessment 2</option>
+              <option value="3">Assessment 3</option>
+            </select>
+
             <button type="submit"
               class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
             <!-- <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
@@ -56,7 +59,8 @@ export default defineComponent({
       </div>
     </div>
   </div>
-  <button class="hover:bg-blue-500 hover:text-white border-blue-500 px-3 py-1 border  rounded text-blue-500" @click="startAssess = true">Start Assessment</button>
+  <button class="hover:bg-blue-500 hover:text-white border-blue-500 px-3 py-1 border  rounded text-blue-500"
+    @click="startAssess = true">Start Assessment</button>
 </template>
 
 

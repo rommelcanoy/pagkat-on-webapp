fa4615457ea7f3b94ec6919efc0a86a6b8f9ed8b
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2022-12-04 14:17:07 +0800

    fix: move interactive materials to student page

--- src/components/StudentNav.vue
+++ src/components/StudentNav.vue
@@ -41,6 +41,10 @@ export default defineComponent({
               class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Home</router-link>
           </li>
           <li>
+            <router-link to="/student/interactive-materials"
+              class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Interactive Materials</router-link>
+          </li>
+          <li>
             <router-link to="/student/details"
               class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Details</router-link>
           </li>

323f9599d40018326df988f04f002ec7d00526f7
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2022-12-04 22:59:52 +0800

    chore: add view student details in student table at admin dashboard

---
+++ src/pages/Dashboard/ViewStudentDetails.vue
@@ -0,0 +1,111 @@
+<script lang="ts">
+import { defineComponent, ref, reactive } from "vue";
+import Sidebar from "../../components/Sidebar.vue";
+import ViewAssessment from "../../components/ViewAssessment.vue";
+
+export default defineComponent({
+  name: "ViewStudentDetails",
+  components: {
+    ViewAssessment,
+  },
+  setup() {
+    return {
+    }
+  }
+})
+
+</script>
+
+<template>
+  <div class="h-screen flex bg-gray-100 text-gray-900 flex-1">
+    <div class="ml-64 flex-1 flex flex-col ">
+      <nav class="flex p-5 border-b w-full fixed bg-white" aria-label="Breadcrumb">
+        <ol class="inline-flex items-center space-x-1 md:space-x-3">
+          <li class="inline-flex items-center">
+            <router-link to="/dashboard/assessments"
+              class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+
+              <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                  d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
+                  clip-rule="evenodd"></path>
+              </svg>
+              View Student Details
+            </router-link>
+
+          </li>
+          <li aria-current="page">
+            <div class="flex items-center">
+              <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clip-rule="evenodd"></path>
+              </svg>
+              <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">Rommel Canoy</span>
+            </div>
+          </li>
+        </ol>
+      </nav>
+
+      <div class="overflow-auto mt-[64px] ">
+        <div class="p-5">
+          <div class="justify-between flex">
+            <h3 class="text-2xl text-gray-700 ">View Student Details</h3>
+            <div class="">
+              <button type="button" @click="$router.go(-1)"
+                class="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-700">
+                Back
+              </button>
+            </div>
+          </div>
+
+          <div class="mt-4">
+            <div class="flex-1 bg-gray-50 h-full py-8 px-12 flex flex-col items-center gap-12 overflow-y-auto">
+              <div class=" bg-white shadow sm:rounded-lg max-w-xl">
+                <div class="px-4 py-5 sm:px-6">
+                  <h3 class="text-lg font-medium leading-6 text-gray-900">Student Information</h3>
+                  <p class="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>
+                </div>
+                <div class="border-t border-gray-200">
+                  <dl>
+                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt class="text-sm font-medium text-gray-500">Full name</dt>
+                      <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Margot Foster</dd>
+                    </div>
+                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt class="text-sm font-medium text-gray-500">Application for</dt>
+                      <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Backend Developer</dd>
+                    </div>
+                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt class="text-sm font-medium text-gray-500">Email address</dt>
+                      <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">margotfoster@example.com</dd>
+                    </div>
+                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt class="text-sm font-medium text-gray-500">Salary expectation</dt>
+                      <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">$120,000</dd>
+                    </div>
+                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt class="text-sm font-medium text-gray-500">About</dt>
+                      <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Fugiat ipsum ipsum deserunt culpa
+                        aute sint
+                        do nostrud anim incididunt cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint.
+                        Sit id
+                        mollit nulla mollit nostrud in ea officia proident. Irure nostrud pariatur mollit ad adipisicing
+                        reprehenderit deserunt qui eu.</dd>
+                    </div>
+                  </dl>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

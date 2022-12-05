ad53abaad0d9674e38fa6d2e30788a516cc6887d
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2022-12-06 02:51:53 +0800

    style: include general navbar in teacher login page

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -1,12 +1,14 @@
 <script lang="ts">
-import {defineComponent, ref} from 'vue'
-import {systemParams, userStore} from "../../store";
+import { defineComponent, ref } from 'vue'
+import GeneralNav from '../../components/GeneralNav.vue';
+import { systemParams, userStore } from "../../store";
 
 export default defineComponent({
-  setup(){
+  components: { GeneralNav },
+  setup() {
     const auth = userStore()
 
-    return{
+    return {
       auth
     }
   },
@@ -15,17 +17,19 @@ export default defineComponent({
 </script>
 
 <template>
-  <section class="bg-gray-50 dark:bg-gray-900">
-    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+  <section class="bg-gray-50 md:h-screen dark:bg-gray-900 flex flex-col">
+    <general-nav />
+    <div class="flex-1 flex flex-col items-center justify-center">
+      <!-- <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
         <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
-        PAGKAT-ON
-      </a>
+        Teacher
+      </a> -->
       <div
         class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-            Sign in
+          <h1
+            class="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            Welcome Teacher
           </h1>
           <form class="space-y-4 md:space-y-6" action="javascript:void(0)" ref="form" @submit="this.auth.login($event)">
             <div>
@@ -54,16 +58,16 @@ export default defineComponent({
                 password?</a>
             </div>
 
-              <button @click="this.$refs.form.submit()"
-                class="my-6 w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-500">Sign
-                in</button>
+            <button @click="this.$refs.form.submit()"
+              class="my-6 w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-500">Sign
+              in</button>
 
-<!--            <router-link to="/register">-->
-<!--              <p class="text-sm font-light text-gray-500 dark:text-gray-400">-->
-<!--                Don’t have an account yet? <a href="#"-->
-<!--                  class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>-->
-<!--              </p>-->
-<!--            </router-link>-->
+            <!--            <router-link to="/register">-->
+            <!--              <p class="text-sm font-light text-gray-500 dark:text-gray-400">-->
+            <!--                Don’t have an account yet? <a href="#"-->
+            <!--                  class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>-->
+            <!--              </p>-->
+            <!--            </router-link>-->
 
           </form>
         </div>
